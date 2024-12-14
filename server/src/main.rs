bd3300472e121926778af6ca1879ce1a5ff55d58
kseen <korenevdn@gmail.com>
2024-12-14 16:46:36 +0300

    Refactor message handling in server implementation for improved readability and maintainability

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,8 @@
-use std::io::{ErrorKind, Read, Write};
+use std::io::{ ErrorKind, Read, Write };
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{ Receiver, Sender };
 
 const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 512;
@@ -24,50 +24,60 @@ fn main() {
             let tx = tx.clone();
             clients.push(socket.try_clone().expect("failed to clone client"));
 
-            thread::spawn(move || loop {
-                let mut buff = vec![0; MSG_SIZE];
+            thread::spawn(move || {
+                loop {
+                    let mut buff = vec![0; MSG_SIZE];
 
-                match socket.read_exact(&mut buff) {
-                    Ok(_) => {
-                        let total_len = u32::from_le_bytes(buff[0..4].try_into().unwrap()) as usize;
-                        if total_len > MSG_SIZE {
-                            // read the rest of the message
-                            let mut rest = vec![0; total_len - MSG_SIZE];
-                            match socket.read_exact(&mut rest) {
-                                Ok(_) => {
-                                    buff.extend(rest);
-                                },
-                                Err(e) => {
-                                    println!("failed to read rest of message: {:?}", e);
-                                    break;
+                    match socket.read_exact(&mut buff) {
+                        Ok(_) => {
+                            let total_len = u32::from_le_bytes(
+                                buff[0..4].try_into().unwrap()
+                            ) as usize;
+                            if total_len > MSG_SIZE {
+                                // read the rest of the message
+                                let mut rest = vec![0; total_len - MSG_SIZE];
+                                match socket.read_exact(&mut rest) {
+                                    Ok(_) => {
+                                        buff.extend(rest);
+                                    }
+                                    Err(e) => {
+                                        println!("failed to read rest of message: {:?}", e);
+                                        break;
+                                    }
                                 }
                             }
+                            let msg = buff.clone();
+
+                            println!("{}: {:?}", addr, msg);
+                            tx.send(msg).expect("failed to send msg to rx");
                         }
-                        let msg = buff.clone();
-
-                        println!("{}: {:?}", addr, msg);
-                        tx.send(msg).expect("failed to send msg to rx");
-                    }, 
-                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-                    Err(_) => {
-                        println!("closing connection with: {}", addr);
-                        break;
+                        Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                        Err(_) => {
+                            println!("closing connection with: {}", addr);
+                            break;
+                        }
                     }
-                }
 
-                sleep();
+                    sleep();
+                }
             });
         }
 
         if let Ok(msg) = rx.try_recv() {
-            clients = clients.into_iter().filter_map(|mut client| {
-                let mut buff = msg.clone();
-                let total_len = u32::from_le_bytes(buff[0..4].try_into().unwrap()) as usize;
-                let packet_len = std::cmp::max(total_len, MSG_SIZE);
-                buff.resize(packet_len, 0);
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone();
+                    let total_len = u32::from_le_bytes(buff[0..4].try_into().unwrap()) as usize;
+                    let packet_len = std::cmp::max(total_len, MSG_SIZE);
+                    buff.resize(packet_len, 0);
 
-                client.write_all(&buff).map(|_| client).ok()
-            }).collect::<Vec<_>>();
+                    client
+                        .write_all(&buff)
+                        .map(|_| client)
+                        .ok()
+                })
+                .collect::<Vec<_>>();
         }
 
         sleep();
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use aes::cipher::{BlockSizeUser, KeySizeUser};
+use aes::cipher::{ BlockSizeUser, KeySizeUser };
 use aes::Aes256;
 use block_padding::Pkcs7;
 use cbc;
-use cipher::{BlockDecryptMut, BlockEncryptMut, KeyInit, KeyIvInit};
+use cipher::{ BlockDecryptMut, BlockEncryptMut, KeyInit, KeyIvInit };
 use clear_on_drop::clear::Clear;
-use generic_array::{typenum::U32, GenericArray};
+use generic_array::{ typenum::U32, GenericArray };
 use hkdf::Hkdf;
 use hmac::digest::OutputSizeUser;
-use hmac::{Hmac, Mac};
-use ksi_double_ratchet::{self as dr, KeyPair as _};
-use rand_core::{CryptoRng, RngCore};
+use hmac::{ Hmac, Mac };
+use ksi_double_ratchet::{ self as dr, KeyPair as _ };
+use rand_core::{ CryptoRng, RngCore };
 use rand_os::OsRng;
 use sha2::Sha256;
 use std::fmt;
-use std::hash::{Hash, Hasher};
-use x25519_dalek::{self, SharedSecret};
+use std::hash::{ Hash, Hasher };
+use x25519_dalek::{ self, SharedSecret };
 
 pub type SignalDR = dr::DoubleRatchet<SignalCryptoProvider>;
 
@@ -42,12 +42,15 @@ impl dr::CryptoProvider for SignalCryptoProvider {
         let mut output_key_material = [0; 64];
         pseudo_random_key.expand(&info, &mut output_key_material).unwrap();
 
-        let root_key: GenericArray<u8, _> = GenericArray::<u8, U32>::from_slice(&output_key_material[..32]).clone();
+        let root_key: GenericArray<u8, _> = GenericArray::<u8, U32>
+            ::from_slice(&output_key_material[..32])
+            .clone();
 
-        let chain_key: GenericArray<u8, _> =
-            GenericArray::<u8, U32>::from_slice(&output_key_material[32..]).clone();
+        let chain_key: GenericArray<u8, _> = GenericArray::<u8, U32>
+            ::from_slice(&output_key_material[32..])
+            .clone();
 
-        return (SymmetricKey(root_key), SymmetricKey(chain_key))
+        return (SymmetricKey(root_key), SymmetricKey(chain_key));
     }
 
     fn kdf_ck(ck: &SymmetricKey) -> (SymmetricKey, SymmetricKey) {
@@ -63,7 +66,7 @@ impl dr::CryptoProvider for SignalCryptoProvider {
         mac.update(&[0x02]);
         let chain_key = mac.finalize().into_bytes();
 
-        return (SymmetricKey(chain_key), SymmetricKey(message_key))
+        return (SymmetricKey(chain_key), SymmetricKey(message_key));
     }
 
     fn encrypt(key: &SymmetricKey, pt: &[u8], ad: &[u8]) -> Vec<u8> {
@@ -74,11 +77,16 @@ impl dr::CryptoProvider for SignalCryptoProvider {
         let mut output_key_material = [0; 80];
         pseudo_random_key.expand(info, &mut output_key_material).unwrap();
 
-        let encryption_key = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(&output_key_material[..32]);
-        let message_key = GenericArray::<u8, <Hmac<Sha256> as OutputSizeUser>::OutputSize>::from_slice(
-            &output_key_material[32..64],
+        let encryption_key = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(
+            &output_key_material[..32]
         );
-        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(&output_key_material[64..]);
+        let message_key = GenericArray::<
+            u8,
+            <Hmac<Sha256> as OutputSizeUser>::OutputSize
+        >::from_slice(&output_key_material[32..64]);
+        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(
+            &output_key_material[64..]
+        );
 
         type Aes256CbcEnc = cbc::Encryptor<Aes256>;
         let ciphr = Aes256CbcEnc::new(encryption_key, iv);
@@ -94,7 +102,7 @@ impl dr::CryptoProvider for SignalCryptoProvider {
         };
 
         let mut mac = <Hmac<Sha256> as Mac>::new_from_slice(message_key).unwrap();
-        
+
         mac.update(ad);
         mac.update(&ciphertext);
 
@@ -118,12 +126,17 @@ impl dr::CryptoProvider for SignalCryptoProvider {
         match pseudo_random_key.expand(info, &mut output_key_material) {
             Ok(_) => (),
             Err(e) => panic!("Error: {:?}", e),
-        };
-        let decryption_key = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(&output_key_material[..32]);
-        let message_key = GenericArray::<u8, <Hmac<Sha256> as OutputSizeUser>::OutputSize>::from_slice(
-            &output_key_material[32..64],
+        }
+        let decryption_key = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(
+            &output_key_material[..32]
         );
-        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(&output_key_material[64..]);
+        let message_key = GenericArray::<
+            u8,
+            <Hmac<Sha256> as OutputSizeUser>::OutputSize
+        >::from_slice(&output_key_material[32..64]);
+        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(
+            &output_key_material[64..]
+        );
 
         let ciphertext_len = ct.len() - 8;
 
@@ -134,10 +147,10 @@ impl dr::CryptoProvider for SignalCryptoProvider {
 
         mac.update(ad);
         mac.update(&ct[..ciphertext_len]);
-        
+
         let tag = mac.finalize().into_bytes();
 
-        if bool::from(!((&tag[..8]) == (&ct[ciphertext_len..]))) {
+        if bool::from(!(&tag[..8] == &ct[ciphertext_len..])) {
             output_key_material.clear();
             println!("Error: {:?}", dr::DecryptError::DecryptFailure);
             return Err(dr::DecryptError::DecryptFailure);
@@ -210,11 +223,7 @@ impl fmt::Debug for KeyPair {
 
     #[cfg(not(debug_assertions))]
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "KeyPair {{ private (bytes): <hidden bytes>, public: {:?} }}",
-            self.public
-        )
+        write!(f, "KeyPair {{ private (bytes): <hidden bytes>, public: {:?} }}", self.public)
     }
 }
 
@@ -262,16 +271,16 @@ fn signal_session() {
     // Copy some values (these are usually the outcome of an X3DH key exchange)
     let bobs_prekey = KeyPair::new(&mut rng);
     let bobs_public_prekey = bobs_prekey.public().clone();
-    let shared = SymmetricKey(GenericArray::<u8, U32>::clone_from_slice(
-        b"Output of a X3DH key exchange...",
-    ));
+    let shared = SymmetricKey(
+        GenericArray::<u8, U32>::clone_from_slice(b"Output of a X3DH key exchange...")
+    );
 
     // Alice fetches Bob's prekey bundle and completes her side of the X3DH handshake
     let mut alice = SignalDR::new_alice(&shared, bobs_public_prekey, None, &mut rng);
 
     // Alice creates her first message to Bob
     let pt_a_0 = b"Hello Bob";
-    
+
     let (h_a_0, ct_a_0) = alice.ratchet_encrypt(pt_a_0, ad_a, &mut rng);
     // Alice creates an initial message containing `h_a_0`, `ct_a_0` and other X3DH information
 
@@ -279,47 +288,30 @@ fn signal_session() {
     let mut bob = SignalDR::new_bob(shared, bobs_prekey, None);
     // Bob can now decrypt the initial message
 
-    assert_eq!(
-        Ok(Vec::from(&b"Hello Bob"[..])),
-        bob.ratchet_decrypt(&h_a_0, &ct_a_0, ad_a)
-    );
+    assert_eq!(Ok(Vec::from(&b"Hello Bob"[..])), bob.ratchet_decrypt(&h_a_0, &ct_a_0, ad_a));
     // Bob is now fully initialized: both sides can send and receive message
 
     let pt_a_1 = b"I will send this later";
     let (h_a_1, ct_a_1) = alice.ratchet_encrypt(pt_a_1, ad_a, &mut rng);
     let pt_b_0 = b"My first reply";
     let (h_b_0, ct_b_0) = bob.ratchet_encrypt(pt_b_0, ad_b, &mut rng);
-    assert_eq!(
-        Ok(Vec::from(&pt_b_0[..])),
-        alice.ratchet_decrypt(&h_b_0, &ct_b_0, ad_b)
-    );
+    assert_eq!(Ok(Vec::from(&pt_b_0[..])), alice.ratchet_decrypt(&h_b_0, &ct_b_0, ad_b));
     let pt_a_2 = b"What a boring conversation";
     let (h_a_2, _ct_a_2) = alice.ratchet_encrypt(pt_a_2, ad_a, &mut rng);
     let pt_a_3 = b"Don't you agree?";
     let (h_a_3, ct_a_3) = alice.ratchet_encrypt(pt_a_3, ad_a, &mut rng);
-    assert_eq!(
-        Ok(Vec::from(&pt_a_3[..])),
-        bob.ratchet_decrypt(&h_a_3, &ct_a_3, ad_a)
-    );
+    assert_eq!(Ok(Vec::from(&pt_a_3[..])), bob.ratchet_decrypt(&h_a_3, &ct_a_3, ad_a));
 
     let pt_b_1 = b"Agree with what?";
     let (h_b_1, ct_b_1) = bob.ratchet_encrypt(pt_b_1, ad_b, &mut rng);
-    assert_eq!(
-        Ok(Vec::from(&pt_b_1[..])),
-        alice.ratchet_decrypt(&h_b_1, &ct_b_1, ad_b)
-    );
+    assert_eq!(Ok(Vec::from(&pt_b_1[..])), alice.ratchet_decrypt(&h_b_1, &ct_b_1, ad_b));
 
-    assert_eq!(
-        Ok(Vec::from(&pt_a_1[..])),
-        bob.ratchet_decrypt(&h_a_1, &ct_a_1, ad_a)
-    );
+    assert_eq!(Ok(Vec::from(&pt_a_1[..])), bob.ratchet_decrypt(&h_a_1, &ct_a_1, ad_a));
 
     // No resending (that key is already deleted)
     assert!(bob.ratchet_decrypt(&h_a_1, &ct_a_1, ad_a).is_err());
     // No fake messages
-    assert!(bob
-        .ratchet_decrypt(&h_a_2, b"Incorrect ciphertext", ad_a)
-        .is_err());
+    assert!(bob.ratchet_decrypt(&h_a_2, b"Incorrect ciphertext", ad_a).is_err());
 
     println!("[singal_session] Session complete");
 }
